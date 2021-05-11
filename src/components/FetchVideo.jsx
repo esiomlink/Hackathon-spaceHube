f92af943c3a9ff4b07c9917f12384a67c76c49f2
anthonyDeplanque <anthonyDeplanque@gmail.com>
2021-05-11 19:43:06 +0200

    add nav buttons

--- src/components/FetchVideo.jsx
+++ src/components/FetchVideo.jsx
@@ -2,19 +2,33 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import CardVideo from './CardVideo';
 import './assets/FetchVideo.css';
-
-
+import NavButton from "./NavButtons";
 
 export default function FetchVideo() {
   const [videos, setVideos] = useState();
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    axios
-      .get("https://images-api.nasa.gov/search?q=rovers&media_type=video")
-      .then((r) => r.data.collection.items)
-      .then((r) => setVideos(r));
-  }, []);
+    fetchApi();
+  }, [page, videos]);
+
+function fetchApi(){
+  axios
+  .get(`https://images-api.nasa.gov/search?page=${page}&media_type=video`)
+  .then((r) => r.data.collection.items)
+  .then((r) => setVideos(r));
+}
 
   if (videos){
-    return <div className="gridVideo">{(videos && videos.map((e) => <CardVideo preview={e}/>))}</div>
+    return (
+    <>
+      <div className="gridVideo">
+        <div >
+          <NavButton page={page} setPage={setPage} />
+        </div>
+        {(videos && videos.map((e) => <CardVideo preview={e}/>))}
+      </div>
+    </>
+    );
   }else return <p>loading</p>;
 }
