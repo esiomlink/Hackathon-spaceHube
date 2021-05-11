1ece4d14dcd0c45a13937d71750f2231523c4497
esiomlink <simon.ecoffet@gmail.com>
2021-05-11 16:03:58 +0200

    resolv css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Home from './views/Home';
 import Navbar from './components/Navbar';
-import FetchVideos from './components/FetchVideo';
+
 
 function App() {
   return (
     <>
     <div className='App'>
-          <FetchVideos />
+          
       <Router>
         <Route path='/' exact component={Home} />
 {/*         <Route path='/jeux' exact component={Carousel} /> */}
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Pub from '../components/Pub.jsx';
 import Navbar from '../components/Navbar.jsx';
+import FetchVideo from '../components/FetchVideo.jsx';
+
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Pub />
+      <FetchVideo/>
     </div>
   );
 }
