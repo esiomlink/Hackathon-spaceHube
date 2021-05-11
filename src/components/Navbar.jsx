842de22ceb8006391c97731ce95cf1a1b88ae99b
Lucciardi <charlotte.lucciardi@gmail.com>
2021-05-11 22:32:40 +0200

    ajout css navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ export default function Navbar(){
   return(
     <nav className="navbar-left">
       <ul className="left-menu"> 
-        <li><Link className="left-text-white" to="/">Catégories</Link ></li>
-        <li><Link className="left-text-white" to="/">Dernières Vidéos</Link ></li>
-        <li><Link className="left-text-white" to="/">Vidéos Premium</Link ></li>
-        <li><Link className="left-text-white" to="/">Acteurs</Link ></li>
+        <li><Link className="left-text-white" to="/">Catégories <i className="fas fa-sort-down fas-arrow"></i></Link ></li>
+        <li><Link className="left-text-white" to="/">Dernières Vidéos<i className="fas fa-sort-down fas-arrow"></i></Link ></li>
+        <li><Link className="left-text-white" to="/">Vidéos Premium<i className="fas fa-sort-down fas-arrow"></i></Link ></li>
+        <li><Link className="left-text-white" to="/">Acteurs<i className="fas fa-sort-down fas-arrow"></i></Link ></li>
       </ul>
     </nav>
   )
