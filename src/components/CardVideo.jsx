b1a2a9f9d28dcfb45ae571752e17080ba0e1e976
SebastienFrancois <sebastien.devlopweb@gmail.com>
2021-05-12 00:07:14 +0200

    useHistory CardVideo

--- src/components/CardVideo.jsx
+++ src/components/CardVideo.jsx
@@ -1,22 +1,23 @@
 import './assets/CardVideo.css';
+import { useHistory } from 'react-router-dom';
 const videoLink = "https://images-assets.nasa.gov/video/";
 
 function CardVideo({preview}) {
-if (preview){
-    const tmpvid = `${videoLink}${preview.data[0].nasa_id}/${preview.data[0].nasa_id}~medium.mp4`;
-    const tmpimg = `${videoLink}${preview.data[0].nasa_id}/${preview.data[0].nasa_id}~medium.jpg`;
-    
+    let history = useHistory();
+    if (preview){
+        const tmpvid = `${videoLink}${preview.data[0].nasa_id}/${preview.data[0].nasa_id}~medium.mp4`;
+        const tmpimg = `${videoLink}${preview.data[0].nasa_id}/${preview.data[0].nasa_id}~medium.jpg`;
+        const OpenDetails = () => {
+          history.push(`play/${tmpvid}`)
+        }
+        
     return (
         <div className="cardContainer">
-             <a href={tmpvid}><img src={tmpimg} className="preview" alt={tmpimg}/></a> 
+             <img onClick={OpenDetails} src={tmpimg} className="preview" alt={tmpimg}/> 
             <div className="infoCard">
-                <a href="#">
-                    <img src={tmpimg} alt="Actor's name"/>
-                </a>
+                    <img onClick={OpenDetails} src={tmpimg} alt="Actor's name"/>
                 <div className="status">
-                <a href={tmpvid}>
-                    <h3 className="name">{preview.data[0].title.split('_').join(' ').split(',').join(' ')}</h3>
-                    </a>
+                    <h3 className="name" onClick={OpenDetails}>{preview.data[0].title.split('_').join(' ').split(',').join(' ')}</h3>
                     <span>{preview.data[0].photographer}</span>
                     {preview.data[0].keywords && <ul>{preview.data[0].keywords.filter((e,i)=>i<3).map(e=><li>{e.split('_').join(' ').split(',').join(' ')}</li>)}</ul>}
                     <span> {preview.data[0].date_created}</span>
