c915d11590f213bc1513a221baa5910100c2cd64
SebastienFrancois <sebastien.devlopweb@gmail.com>
2021-05-12 12:03:50 +0200

    Title Categories

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {useParams} from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import CardVideo from "./CardVideo";
 import NavButton from "./NavButtons";
-import './assets/FetchVideo.css';
-import './assets/NavButtons.css';
+import "./assets/FetchVideo.css";
+import "./assets/NavButtons.css";
 
 function Category() {
   const [videos, setVideos] = useState();
@@ -23,14 +23,31 @@ function Category() {
       .then((r) => r.data.collection.items)
       .then((r) => setVideos(r));
   }
-
+  let str = "";
+  switch (cat ) {
+    case 'tank':
+      str = "Gros réservoir"
+      break;
+    case 'apollo 11':
+      str = "RILF"
+      break;
+    case 'engine':
+      str = "Joint de cullase"
+      break;
+    default:
+      str = "Uranus"
+  }
   return (
     <>
       <div className="button-container">
         <NavButton page={page} setPage={setPage} />
       </div>
+       <h1 style={{textAlign:'center', fontSize: '2rem', margin:'1rem'}}>{str}</h1>
       <div className="gridVideo">
         {videos && videos.map((e) => <CardVideo preview={e} />)}
+      </div>
+      <div className="button-container">
+        <NavButton page={page} setPage={setPage} />
       </div>
     </>
   );
